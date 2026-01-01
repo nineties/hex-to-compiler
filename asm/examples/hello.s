966969a5b219c27b2d27f712fbb0edaaa032b925
Koichi Nakamura <koichi@idein.jp>
2026-01-01 20:47:45 +0900

    Add asm/examples/hello.s

---
+++ asm/examples/hello.s
@@ -0,0 +1,23 @@
+; hello.s
+
+(global _start)
+
+(section data)
+(label msg
+    (.ascii "Hello World!\n")
+    )
+
+(def len (- here msg))
+
+(section text)
+(label _start
+    (mov 1 %rax)                ; SYS_WRITE
+    (mov 1 %rdi)                ; stdout
+    (lea (mem msg %rip) %rsi)   ; addr
+    (mov len %rdx)              ; length
+    (syscall)
+
+    (mov 60 %rax)   ; SYS_EXIT
+    (xor %rdi %rdi) ; exit code 0
+    (syscall)
+    )
