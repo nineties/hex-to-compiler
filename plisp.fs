39436671756d83b9477a7971cdfbd79e94616431
Koichi Nakamura <koichi@idein.jp>
2026-01-03 21:45:36 +0900

    add int?

--- plisp.fs
+++ plisp.fs
@@ -2062,6 +2062,7 @@ s" car"  :noname car ; add-prim
 s" cdr"  :noname cdr ; add-prim
 s" setcar" :noname swap node>arg0 ! nil ; add-prim
 s" setcdr" :noname swap node>arg1 ! nil ; add-prim
+s" int?" :noname node>type @ Nint = if Strue else nil then ; add-prim
 s" nil?" :noname nil = if Strue else nil then ; add-prim
 s" cons?" :noname node>type @ Ncons = if Strue else nil then ; add-prim
 s" sym?"  :noname node>type @ Nsymbol = if Strue else nil then ; add-prim
