698b0a2f652782be1c398f08c75cb1e0799bc917
Koichi Nakamura <koichi@idein.jp>
2026-01-12 19:02:56 +0900

    plisp2: wrote allocate

--- plisp2.sv
+++ plisp2.sv
@@ -4,14 +4,38 @@
 (include "std.sv")
 
 (def HEAP_BLOCK_SIZE (* 128 (* 1024 1024))) ; 128 MB
-(long heap_block)
+(long heap_root)
+(long heap_end)
+(long heap_pos)
 (fun init_heap ()
     (var addr (mmap2 0 HEAP_BLOCK_SIZE
         (| PROT_READ PROT_WRITE)
         (| MAP_PRIVATE MAP_ANONYMOUS)
         -1 0))
-    (fprint_int STDOUT addr 16)
+    (if (u>= addr 0xfffff001) (do
+        (eputs "mmap2 failed\n")
+        (exit 1)
+        ))
+    (= heap_root addr)
+    (= heap_pos addr)
+    (= heap_end (+ addr HEAP_BLOCK_SIZE))
     )
+
+(fun align (n) ; align n to 4-byte boundary
+    (return (& (+ n 3) 0xfffffffe))
+    )
+
+(fun allocate (size)
+    (= size (align size))
+    (if (>= (+ heap_pos size) heap_end) (do
+        (eputs "memory allocation error\n")
+        (exit 1)
+        ))
+    (var addr heap_pos)
+    (+= heap_pos size)
+    (return addr)
+    )
+
 
 (fun read_file (path)
     (var fd (open path O_RDONLY))
@@ -23,6 +47,9 @@
     (if (< file_size 0) (do
         (eputs "fstat failed: ") (eputs path) (eputs "\n")
         (exit 1)))
+
+    (var buf (allocate file_size))
+    (var r (read fd buf file_size))
 
     (close fd)
     )
@@ -38,7 +65,6 @@
         ))
 
     (init_heap)
-
 
     (var sexp_list (read_sexp_list (get argv 1)))
     )
